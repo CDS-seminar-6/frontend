3aa28db4efdd7a331f5b17e9a17aaa460c3e46d2
sunny <huiseon37@gmail.com>
2022-05-15 21:59:36 +0900

    css: :art: root margin 변경

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -6,18 +6,10 @@ const GlobalStyle = createGlobalStyle`
 
   @font-face {font-family: "SF Pro Display"; src: url("//db.onlinewebfonts.com/t/179429c3795fb9aed42c526bd1133cc0.eot"); src: url("//db.onlinewebfonts.com/t/179429c3795fb9aed42c526bd1133cc0.eot?#iefix") format("embedded-opentype"), url("//db.onlinewebfonts.com/t/179429c3795fb9aed42c526bd1133cc0.woff2") format("woff2"), url("//db.onlinewebfonts.com/t/179429c3795fb9aed42c526bd1133cc0.woff") format("woff"), url("//db.onlinewebfonts.com/t/179429c3795fb9aed42c526bd1133cc0.ttf") format("truetype"), url("//db.onlinewebfonts.com/t/179429c3795fb9aed42c526bd1133cc0.svg#SF Pro Display") format("svg"); }
 
-  html{
-    display: flex;
-    justify-content: center;
-    width:100%;
-    height: 100%;
-    font-size: 62.5%;
-    font-family: "SF Pro Display";
-  }
-
-  #root, body {
+  #root, body,html {
     width: 375px;
     height: 100%;
+    margin: 0 auto;
     font-size: 62.5%;
     font-family: "SF Pro Display";
   }
