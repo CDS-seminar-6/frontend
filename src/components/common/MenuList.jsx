9678f82a06cb7c8b6a172b45c3d39a80c3fe0bd0
say-young516 <sychoi0516@naver.com>
2022-05-21 02:08:02 +0900

    feat: :sparkles:메뉴 컨테이너, 메뉴 버튼 생성

--- src/components/common/MenuList.jsx
+++ src/components/common/MenuList.jsx
@@ -1,7 +1,28 @@
 import { React } from 'react';
 import styled from 'styled-components';
 
+const MenuContainer = styled.div`
+  width: 34.3rem;
+  margin: 1.6rem;
+  display: flex;
+  flex-direction: column;
+`;
+const MenuBtn = styled.button`
+  width: 34.3rem;
+  height: 5rem;
+  margin-bottom: 1rem;
+  padding: 0;
+  border: none;
+  background: none;
+  color: ${({ theme }) => theme.colors.surfit_white};
+`;
 function MenuList() {
-  return <></>;
+  return (
+    <MenuContainer>
+      <MenuBtn>서핏 프로필</MenuBtn>
+      <MenuBtn>커리어 관리</MenuBtn>
+      <MenuBtn>설정</MenuBtn>
+    </MenuContainer>
+  );
 }
 export default MenuList;
