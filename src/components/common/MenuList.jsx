96bd3363999639215040b4f82b150a0f6bd3dd42
say-young516 <sychoi0516@naver.com>
2022-05-21 02:39:16 +0900

    feat: :sparkles: 메뉴버튼 아이콘 삽입 및 스타일 설정

--- src/components/common/MenuList.jsx
+++ src/components/common/MenuList.jsx
@@ -1,5 +1,29 @@
+import bag from 'assets/icon_bag.svg';
+import setting from 'assets/icon_setting.svg';
+import surfing from 'assets/icon_surfing.svg';
 import { React } from 'react';
 import styled from 'styled-components';
+import { theme } from 'styles/theme';
+
+function MenuList() {
+  return (
+    <MenuContainer>
+      <MenuBtn>
+        <MenuIcon src={surfing} alt="서핏프로필 아이콘" />
+        서핏 프로필
+      </MenuBtn>
+      <MenuBtn>
+        <MenuIcon src={bag} alt="커리어관리 아이콘" />
+        커리어 관리
+      </MenuBtn>
+      <MenuBtn>
+        <MenuIcon src={setting} alt="설정 아이콘" />
+        설정
+      </MenuBtn>
+    </MenuContainer>
+  );
+}
+export default MenuList;
 
 const MenuContainer = styled.div`
   width: 34.3rem;
@@ -12,17 +36,15 @@ const MenuBtn = styled.button`
   height: 5rem;
   margin-bottom: 1rem;
   padding: 0;
+  font-size: 1.6rem;
+  font-weight: bold;
   border: none;
   background: none;
-  color: ${({ theme }) => theme.colors.surfit_white};
+  color: ${theme.colors.surfit_white};
+  display: flex;
+  align-items: center;
 `;
-function MenuList() {
-  return (
-    <MenuContainer>
-      <MenuBtn>서핏 프로필</MenuBtn>
-      <MenuBtn>커리어 관리</MenuBtn>
-      <MenuBtn>설정</MenuBtn>
-    </MenuContainer>
-  );
-}
-export default MenuList;
+const MenuIcon = styled.img`
+  display: inline-block;
+  margin: 1.3rem 0.8rem 1.3rem 1.2rem;
+`;
