216ac4b58fc4d556f9aca446871d922929073c87
say-young516 <sychoi0516@naver.com>
2022-05-21 13:26:47 +0900

    chore: :gear:svg 적용안돼서파일 변경

--- src/components/surfitProfile/DropDownBtn.jsx
+++ src/components/surfitProfile/DropDownBtn.jsx
@@ -1,12 +1,38 @@
+import arrow_down from 'assets/icon_arrow_down.svg';
 import MenuList from 'components/common/MenuList';
 import React from 'react';
 import styled from 'styled-components';
 import { theme } from 'styles/theme';
-
-function DropDownBtn(){
-    return(
-        
-    );
+function DropDownBtn() {
+  return (
+    <StyledBtn>
+      <span>서핏 프로필</span>
+      <StyledIcon src={arrow_down} alt="드롭다운 버튼 이미지" />
+    </StyledBtn>
+  );
 }
-
 export default DropDownBtn;
+const StyledBtn = styled.button`
+  width: 34.3rem;
+  height: 6.3rem;
+  margin: 1.6rem;
+  padding: 0;
+  border-radius: 1.2rem;
+  border: none;
+  background: ${theme.colors.surfit_black_2};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  & > span {
+    diplay: inline-block;
+    color: ${theme.colors.surfit_white};
+    font-size: 1.6rem;
+    font-weight: bold;
+    margin-left: 1.8rem;
+  }
+`;
+const StyledIcon = styled.img`
+  display: inline-block;
+  margin: 2rem 1.8rem 1.9rem 0rem;
+`;
--- src/pages/SurfitProfile.jsx
+++ src/pages/SurfitProfile.jsx
@@ -1,7 +1,25 @@
+import DropDownBtn from 'components/surfitProfile/DropDownBtn';
 import React from 'react';
-
+import styled from 'styled-components';
+import { theme } from 'styles/theme';
 function SurfitProfile() {
-  return <div>서핏프로필</div>;
+  return (
+    <StyledRoot>
+      서핏프로필
+      <DropDownBtn />
+    </StyledRoot>
+  );
 }
 
 export default SurfitProfile;
+
+const StyledRoot = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 8rem;
+  width: 100%;
+  height: 100%;
+  background-color: ${theme.colors.surfit_black_1};
+`;
