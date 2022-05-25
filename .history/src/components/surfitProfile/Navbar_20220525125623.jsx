d5772b1ae6f3d6d75ebeedd98973743ab1deac9d
Namjun Kim <wrasf175@naver.com>
2022-05-25 18:29:10 +0900

    ⚙️ chore : history folder delete

--- .history/src/components/surfitProfile/Navbar_20220525125623.jsx
+++
@@ -1,6 +0,0 @@
-import bag from 'assets/icon_bag.svg';
-import setting from 'assets/icon_setting.svg';
-import surfing from 'assets/icon_surfing.svg';
-import { React } from 'react';
-import styled from 'styled-components';
-import { theme } from 'styles/theme';
--- .history/src/pages/SurfitProfile_20220518212812.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function SurfitProfile() {
-  return <div>서핏프로필</div>;
-}
-
-export default SurfitProfile;
