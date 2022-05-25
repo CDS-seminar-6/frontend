451b0d1139d1944ed30ee2c5d49927a2bedf0076
Namjun Kim <wrasf175@naver.com>
2022-05-25 20:10:51 +0900

    🔥 : history delete

--- src/pages/SurfitProfile.jsx
+++ src/pages/SurfitProfile.jsx
@@ -1,7 +1,4 @@
-import Navbar from 'components/surfitProfile/Navbar';
 import React from 'react';
-import styled from 'styled-components';
-import { theme } from 'styles/theme';
 
 function SurfitProfile() {
   return <></>;
