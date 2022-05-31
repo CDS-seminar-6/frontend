89853041c82b0c19e5e2f3095571983cbb0c6fdf
sunny <huiseon37@gmail.com>
2022-06-01 02:48:04 +0900

    feat: :sparkles: baseUrl 수정

--- src/cores/api.js
+++ src/cores/api.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export const client = axios.create({
-  baseURL: 'https://asia-northeast3-joint-seminar15-e35f5.cloudfunctions.net/api',
+  baseURL: '/api',
+  header: {
+    'Content-Type': 'application/json',
+  },
 });
